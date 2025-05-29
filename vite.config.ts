1c160b02635cf68f86461e54042edf640cd59f4c
ymmooot <ryota.yama.9+github@gmail.com>
2025-05-29 17:01:37 +0900

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "npm:vite";
 import vue from "npm:@vitejs/plugin-vue";
 
 export default defineConfig({
+  base: "weather-portal",
   plugins: [vue()],
   server: {
     port: 8000,
