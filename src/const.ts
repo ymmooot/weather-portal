985db8dec1592fe412dd9cca680d5e05b1ea16ae
ymmooot <ryota.yama.9+github@gmail.com>
2025-06-06 23:53:03 +0900

    Update cache key

--- src/const.ts
+++ src/const.ts
@@ -1,6 +1,6 @@
 export const favCacheKey = "fav_v2";
 export const searchQueryCacheKey = "search_history_v2";
-export const searchResultCacheKey = "search_location_cache_v2";
+export const searchResultCacheKey = "search_location_cache_v3";
 
 export const allCacheKeys = [
   favCacheKey,
